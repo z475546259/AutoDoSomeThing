9595af79bad1990bf4de462700ba2add53deb0b6
张治强 <zhangzhiqianga@zbj.com>
2017-11-21 21:15:41 +0800

    提交 登录、浏览、收藏

--- src/main/java/cainiaolicai/cnlc_login.java
+++ src/main/java/cainiaolicai/cnlc_login.java
@@ -1,6 +1,23 @@
 package cainiaolicai;
 
-import util.HttpRequestUtil;
+import java.io.UnsupportedEncodingException;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import util.HttpClientRequest;
+import util.HttpClientUtil;
+import util.HttpRequest;
 
 public class cnlc_login {
 	public static void main(String[] args) {
@@ -8,8 +25,102 @@ public class cnlc_login {
 		aa.login();
 	}
 	public void login(){
-		 String sr= HttpRequestUtil.sendPost("http://app.cainiaolc.com/user/login ","telephone=15923584508&password=d5c91303b3963ea463d4d97b616f06224f2469bdb4d9984ca696dd37c7059a7b",true);
-		 System.out.println(sr);
+		HttpClientUtil httpUtil = new HttpClientUtil();
+		Map<String,String> para = new HashMap<String, String>();
+		para.put("telephone", "15923584508");
+		para.put("password", "d5c91303b3963ea463d4d97b616f06224f2469bdb4d9984ca696dd37c7059a7b");
+		//登录
+		String login_res = httpUtil.doPost("http://app.cainiaolc.com/user/login", para, "utf-8");
+		System.out.println(login_res);
+		//查看菜点
+		String coin_userSumary = httpUtil.doGet("http://app.cainiaolc.com/coin/userSummary", "utf-8");
+		System.out.println(coin_userSumary);
+		//主页
+		String api_homeData = httpUtil.doGet("http://app.cainiaolc.com/api/homeData", "utf-8");
+		System.out.println(api_homeData);
+		List<String> ids = new ArrayList<String>();
+		getIDs(api_homeData,ids);
+		
+//        for (String id : ids) {
+//        	//查看文章
+//        	String article_detailSimple = httpUtil.doGet("http://app.cainiaolc.com/article/detailSimple?id="+id, "utf-8");
+//    		System.out.println(article_detailSimple);
+//    		String coin_userSumary2 = httpUtil.doGet("http://app.cainiaolc.com/coin/userSummary", "utf-8");
+//    		System.out.println(coin_userSumary2);
+//    		try {
+//				Thread.sleep(10000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+        //发帖
+//        String forum_content = "你们说一个人的资金大概分散投资到多少个平台合适？";
+//        Map<String,String> forum_para = new HashMap<String, String>();
+//        try {
+////			forum_para.put("content-disposition: form-data; name=\"content\"\r\n" + 
+////					"Content-Length: "+forum_content.getBytes("utf-8").length, forum_content);
+////			forum_para.put("content-disposition: form-data; name=\"category\"\r\n" + 
+////		        		"Content-Length: 3", "p2p");
+////		    forum_para.put("content-disposition: form-data; name=\"cateId\"\r\n" + 
+////		    		"Content-Length: 6", "225410");
+////		    forum_para.put("content-disposition: form-data; name=\"upload\"\r\n" + 
+////		    		"Content-Length: 1", "0");
+//		    forum_para.put("content", forum_content);
+//			forum_para.put("category", "p2p");
+//		    forum_para.put("cateId", "225410");
+//		    forum_para.put("upload", "0");
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//       
+//        String forum_post = httpUtil.doFormPost("http://app.cainiaolc.com/forum/post", forum_para, "utf-8");
+//		System.out.println(forum_post);
+//		String coin_userSumary3 = httpUtil.doGet("http://app.cainiaolc.com/coin/userSummary", "utf-8");
+//		System.out.println(coin_userSumary3);
+		//收藏文章
+		int randomId = (new Random()).nextInt(ids.size());
+		String article_detailSimple = httpUtil.doGet("http://app.cainiaolc.com/article/favor?id="+ids.get(randomId)+"&status=1", "utf-8");
+		System.out.println(article_detailSimple);
+		String coin_userSumary4 = httpUtil.doGet("http://app.cainiaolc.com/coin/userSummary", "utf-8");
+		System.out.println(coin_userSumary4);
+		//分享文章
+		for(int i=0;i<2;i++) {
+			int shareRandomId = (new Random()).nextInt(ids.size());
+			Map<String,String> share = new HashMap<String, String>();
+			System.out.println(ids.get(shareRandomId));
+			share.put("path", "/articleShare");
+			share.put("referer", "/article/"+ids.get(shareRandomId));
+			String share_result1 = httpUtil.doPost("http://app.cainiaolc.com/log/menuClick", para, "utf-8");
+			share.clear();
+			share.put("fk", ids.get(shareRandomId));
+			share.put("type", "7");
+			String share_result2 = httpUtil.doPost("http://app.cainiaolc.com/log/articleShare", para, "utf-8");
+			System.out.println(share_result1 + " and "+share_result2);	
+			String coin_userSumary5 = httpUtil.doGet("http://app.cainiaolc.com/coin/userSummary", "utf-8");
+			System.out.println(coin_userSumary5);
+		}
+		
+	}
+	public void getIDs(String api_homeData,List<String> ids) {
+		JSONObject json1 = JSONObject.parseObject(api_homeData);
+		JSONObject json1_data = (JSONObject)json1.get("Data");
+		JSONArray  articles = json1_data.getJSONArray("articles");
+//		JSONArray  carousels = json1_data.getJSONArray("carousels");
+//		JSONArray  posts = json1_data.getJSONArray("posts");
+//		JSONArray  refers = json1_data.getJSONArray("refers");
+		getJsonID(articles, ids);
+//		getJsonID(carousels, ids);
+//		getJsonID(posts, ids);
+//		getJsonID(refers, ids);
+	}
+	
+	public void getJsonID(JSONArray  jsonArray,List<String> ids) {
+		for (Iterator iterator = jsonArray.iterator(); iterator.hasNext();) { 
+        	JSONObject job = (JSONObject) iterator.next(); 
+        	ids.add(job.get("id").toString());
+        }
 	}
 	
 }
