4f8d90ce1c194389c77a71251b1b035fc5c68c16
张治强 <zhangzhiqianga@zbj.com>
2018-05-08 18:22:43 +0800

    修改为从数据库读取user，并增加随机 deviceid和user_agent

--- src/main/java/util/test.java
+++ src/main/java/util/test.java
@@ -1,15 +1,14 @@
 package util;
 
-import java.io.UnsupportedEncodingException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import cainiaolicai.cnUser;
 
 public class test {
 	public static void main(String[] args) {
 		OperateOracle operateOracle = new OperateOracle();
 //		operateOracle.AddData(1,"菜鸟理财");
-		operateOracle.updateAppData("菜鸟理财","张治强","15923584508","","475546259",200,"16284");
+		cnUser user = new cnUser();
+		user.setUser_name("张治强");
+		user.setTelephone("15923584508");
+		operateOracle.updateAppData("菜鸟理财",user,200);
 	}
 }
