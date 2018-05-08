d9818b43097088124bd63d10edaf7dd5240f4206
z475546259 <244768284@qq.com>
2018-05-08 20:49:24 +0800

    暂存

--- src/main/java/util/test.java
+++ src/main/java/util/test.java
@@ -10,6 +10,6 @@ public class test {
 	public static void main(String[] args) {
 		OperateOracle operateOracle = new OperateOracle();
 //		operateOracle.AddData(1,"菜鸟理财");
-		operateOracle.updateAppData("菜鸟理财","张治强","15923584508","","475546259",200,"16284");
+		operateOracle.updateAppData("菜鸟理财","易码13","15584451604","","d5c91303b3963ea463d4d97b616f06224f2469bdb4d9984ca696dd37c7059a7b",100,"16284");
 	}
 }
